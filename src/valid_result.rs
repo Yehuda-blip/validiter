80a0147a3ded23250eb50e3743fe94064b5d2ef3
Yehuda-blip <yehudak12321@gmail.com>
2024-01-02 21:02:50 +0200

    impl Error for ValidErr (C-GOOD-ERR)

--- src/valid_result.rs
+++ src/valid_result.rs
@@ -1,3 +1,5 @@
+use std::{error::Error, fmt::{Debug, Display}};
+
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(Debug, PartialEq)]
 pub enum ValidErr<E> {
@@ -9,5 +11,22 @@ pub enum ValidErr<E> {
     LookBackFailed(E),
     BrokenConstant(E),
 }
+
+impl<E> Display for ValidErr<E> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let err_type_str = match self {
+            ValidErr::TooMany(_) => "ValidErr::TooMany",
+            ValidErr::TooFew => "ValidErr::TooFew",
+            ValidErr::OutOfBounds(_) => "ValidErr::OutOfBounds",
+            ValidErr::Invalid(_) => "ValidErr::Invalid",
+            ValidErr::Lifted => "ValidErr::Lifted",
+            ValidErr::LookBackFailed(_) => "ValidErr::LookBackFailed",
+            ValidErr::BrokenConstant(_) => "ValidErr::BrokenConstant"
+        };
+        write!(f, "{:?}", err_type_str)
+    }
+}
+
+impl<E> Error for ValidErr<E> where E: Debug {}
 
 pub type VResult<E> = Result<E, ValidErr<E>>;
