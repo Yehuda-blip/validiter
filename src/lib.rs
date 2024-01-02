f63e7c73f6194b40e28c170e0c7ea627044aab33
Yehuda-blip <yehudak12321@gmail.com>
2024-01-02 14:37:20 +0200

    add const over and lookback to the adapter chain test

--- src/lib.rs
+++ src/lib.rs
@@ -18,8 +18,10 @@ mod tests {
 
     #[test]
     fn test_multi_validation_on_iterator() {
-        let validation_results = (0..10)
+        let validation_results = (0..10).chain(0..10)
             .validate()
+            .look_back_n::<10, _, _, _>(|i| *i, |prev, curr| prev == curr)
+            .const_over(|i| *i >= 0)
             .at_most(7)
             .between(2, 8)
             .ensure(|i| i % 2 == 0)
@@ -35,6 +37,16 @@ mod tests {
                 Ok(4),
                 Err(ValidErr::Invalid(5)),
                 Ok(6),
+                Err(ValidErr::TooMany(7)),
+                Err(ValidErr::TooMany(8)),
+                Err(ValidErr::TooMany(9)),
+                Err(ValidErr::TooMany(0)),
+                Err(ValidErr::TooMany(1)),
+                Err(ValidErr::TooMany(2)),
+                Err(ValidErr::TooMany(3)),
+                Err(ValidErr::TooMany(4)),
+                Err(ValidErr::TooMany(5)),
+                Err(ValidErr::TooMany(6)),
                 Err(ValidErr::TooMany(7)),
                 Err(ValidErr::TooMany(8)),
                 Err(ValidErr::TooMany(9)),
