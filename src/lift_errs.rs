ed4a7e7d9cf4d83aec9cde122338432d7879569e
Yehuda-blip <yehudak12321@gmail.com>
2024-01-04 15:02:04 +0200

    links in lift errs

--- src/lift_errs.rs
+++ src/lift_errs.rs
@@ -40,26 +40,26 @@ where
 }
 
 /// The trait defining iterators that can be transformed into
-/// a `ValidIter` without calling `validate`. While it is not
+/// a [`ValidIter`](ValidIter) without calling [`validate`](crate::Unvalidatable::validate). While it is not
 /// sealed, you should probably not implement it unless you
 /// want to experiment.
 pub trait ErrLiftable<OkType, ErrType>:
     Iterator<Item = Result<OkType, ValidErr<ErrType>>> + Sized
 {
     /// Turns an iterator over `Result<OkType, ValidErr<ErrType>>>`
-    /// into a `ValidIter` over `VResult<OkType>` by dropping all
-    /// the `ValidErr<ErrType>` elements and replacing them with
-    /// `ValidErr<OkType>::Lifted`.
+    /// into a [`ValidIter`] over [`VResult<OkType>`] by dropping all
+    /// the [`ValidErr<ErrType>`] elements and replacing them with
+    /// [`ValidErr<OkType>::Lifted`].
     ///
     /// `lift_errs` is useful in 2 scenarios:
     /// 1. When some opertion on the iterator
     /// causes a change in the underlying type of element (usually,
     /// collecting an iterator).
     /// 2. When as a result of err mapping, the underlying type is
-    /// a `VResult` but the iterator itself is not a `ValidIter`.
+    /// a [`VResult`] but the iterator itself is not a [`ValidIter`].
     ///
-    /// It's purpose is similar to the `validate` method - sending
-    /// an iterator to the `ValidIter` type.
+    /// It's purpose is similar to the [`validate`] method - sending
+    /// an iterator to the [`ValidIter`] type.
     ///
     /// This would be better explained with an example:
     /// # Examples
@@ -87,6 +87,10 @@ pub trait ErrLiftable<OkType, ErrType>:
     /// assert_eq!(mat, Err(ValidErr::Lifted)); // the element at pos [1][1] would have been negative, failing the ensure validation
     ///
     /// ```
+    /// 
+    /// [`VResult<OkType>`](crate::valid_result::VResult)
+    /// [`VResult`](crate::valid_result::VResult)
+    /// [`validate`](crate::valid_iter::Unvalidatable::validate) 
     fn lift_errs(self) -> LiftErrs<OkType, ErrType, Self> {
         LiftErrs::new(self)
     }
